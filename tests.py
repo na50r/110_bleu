de40385ed25a921f5a8e4415683cf48a578c831f
na50r <shanrosan@gmail.com>
2025-04-19 17:33:36 +0200

    update tests

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 from scripts.data_management import FloresPlusManager, Opus100Manager, EPManager
 import random
-# Run tests with: python -m pytest --capture=tee-sys tests.py
+# https://stackoverflow.com/questions/14405063/how-to-see-normal-stdout-stderr-console-print-output-from-code-during-a-pytest
+# Run tests with: python -m pytest -s tests.py
 
 def test_flores_1():
     dm = FloresPlusManager()
@@ -9,23 +10,27 @@ def test_flores_1():
     out = dm.get_sentences(lang, num_of_sents=expected)
     assert len(out[lang]) == expected
 
+
 def test_flores_2():
     dm = FloresPlusManager()
     expected = random.randint(0, 100)
     src_lang, tgt_lang = random.sample(sorted(FloresPlusManager.EURO_LANGS), 2)
-    src_sents, tgt_sents = dm.get_sentence_pairs(src_lang, tgt_lang, num_of_sents=expected)
+    src_sents, tgt_sents = dm.get_sentence_pairs(
+        src_lang, tgt_lang, num_of_sents=expected)
     assert len(src_sents) == expected
     assert len(tgt_sents) == expected
+
 
 def test_opus_1():
     dm = Opus100Manager()
     expected = random.randint(0, 100)
     lang = random.sample(sorted(Opus100Manager.EURO_LANGS), k=1)[0]
-    src_sents, tgt_sents = dm.get_sentence_pairs(lang, 'en', num_of_sents=expected)
+    src_sents, tgt_sents = dm.get_sentence_pairs(
+        lang, 'en', num_of_sents=expected)
     assert len(src_sents) == expected
     assert len(tgt_sents) == expected
-    
-    
+
+
 def test_ep_1():
     dm = EPManager()
     expected = random.randint(0, 100)
