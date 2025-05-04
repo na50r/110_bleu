6534d27ce5f23872b170dbfae04df6e36d8a37f4
na50r <shanrosan@gmail.com>
2025-05-04 13:30:00 +0200

    refactor tests

--- test_task_and_logger.py
+++ test_task_and_logger.py
@@ -1,8 +1,8 @@
 from scripts.translators import MockClient
 from scripts.task import TranslationTask
-from scripts.logger import MyLogger, RetryLog
+from scripts.logger import TranslationLogger, RetryLog
 from scripts.data_management import Opus100Manager, EuroParlManager
-from random import choice
+from random import choice, sample
 from io import StringIO
 import os
 import shutil
@@ -19,10 +19,10 @@ def silent_task_run(func):
     sys.stdout = original_stdout
 
 
-def get_pairs(num):
-    pairs = Opus100Manager.EURO_ISO_2_PAIR
-    pairs = [tuple(pair.split('-')) for pair in pairs.values()]
-    return pairs[:num]
+def get_sample_pairs(dm, k=2):
+    pairs = dm.get_pairs()
+    pairs = sample(pairs, k=k)
+    return pairs
 
 
 def setup(foldername):
@@ -44,77 +44,37 @@ def setup_teardown(foldername, func):
         teardown(foldername)
 
 
-def task_run(pairs, mt_folder):
+def task_run(mt_folder):
+    # Generic, random task run on 400 sentences
     dms = [EuroParlManager, Opus100Manager]
-    dm = choice(dms)()
+    dm = choice(dms)
+    pairs = get_sample_pairs(dm)
+    dm = dm()
     logfile = StringIO()
-    logger = MyLogger(logfile=logfile)
+    logger = TranslationLogger(logfile=logfile)
     cli = MockClient(logger=logger)
-
     task = TranslationTask(
         target_pairs=pairs,
         dm=dm,
         client=cli,
         logger=logger,
         mt_folder=mt_folder,
-        num_of_sents=50,
+        num_of_sents=400,
     )
     silent_task_run(task.run)
 
 
 def test_task():
     test_folder = 'tmp_test'
-    pairs = get_pairs(3)
-    setup_teardown(test_folder, lambda: task_run(
-        pairs=pairs, mt_folder=test_folder))
+    setup_teardown(test_folder, lambda: task_run(mt_folder=test_folder))
 
 
-def test_task_with_error_and_retry():
+def test_retry_and_fail_files():
     test_folder = 'tmp_test'
-    pairs = get_pairs(4)
+    pairs = get_sample_pairs(Opus100Manager, k=4)
     dm = Opus100Manager()
     logfile = StringIO()
-    logger = MyLogger(logfile=logfile)
-    cli = MockClient(logger=logger, planned_errors=[
-                     pairs[0], pairs[1], pairs[3]], planned_fails=[pairs[1], pairs[1], pairs[3]])
-
-    task = TranslationTask(
-        target_pairs=pairs,
-        dm=dm,
-        client=cli,
-        logger=logger,
-        mt_folder=test_folder,
-        num_of_sents=400,
-        max_retries=2,
-        retry_delay=0
-    )
-    setup_teardown(test_folder, lambda: silent_task_run(task.run))
-    logvalues = logfile.getvalue()
-    log_data = [json.loads(ln) for ln in logvalues.splitlines()]
-    expected_logs = 4+6-1
-    # with zero failure = 4 logs
-    # with max_retries=2, max 4 * 3 = 12 logs
-    assert len(log_data) == expected_logs
-    expected_fail_logs = 6
-    expected_error_logs = 3
-    actual_fail_logs = 0
-    actual_error_logs = 0
-    for log in log_data:
-        if 'failure' in log['verdict']:
-            actual_fail_logs += 1
-        if 'error' in log['verdict']:
-            actual_error_logs += 1
-            assert log['verdict']['error'] == 'MockError'
-    assert actual_fail_logs == expected_fail_logs
-    assert actual_error_logs == expected_error_logs
-
-
-def test_logging_and_retry():
-    test_folder = 'tmp_test'
-    pairs = get_pairs(4)
-    dm = Opus100Manager()
-    logfile = StringIO()
-    logger = MyLogger(logfile=logfile)
+    logger = TranslationLogger(logfile=logfile)
     cli = MockClient(logger=logger, planned_fails=[
                      pairs[1], pairs[1], pairs[3]])
 
@@ -138,26 +98,15 @@ def test_logging_and_retry():
     assert f'{pairs[1][0]}-{pairs[1][1]}.txt' not in files
     teardown(test_folder)
 
-    logvalues = logfile.getvalue()
-    log_data = [json.loads(ln) for ln in logvalues.splitlines()]
-    expected_logs = len(pairs) + 2
-    assert len(log_data) == expected_logs
 
-    expected_fail_logs = 3
-    actual_fail_logs = 0
-    for log in log_data:
-        if 'failure' in log['verdict']:
-            actual_fail_logs += 1
-    assert actual_fail_logs == expected_fail_logs
-
-
-def test_logging_with_manual_retry():
+def test_meta_data_in_log():
     test_folder = 'tmp_test'
-    pairs = get_pairs(4)
+    pairs = get_sample_pairs(Opus100Manager, k=4)
     dm = Opus100Manager()
     logfile = StringIO()
-    logger = MyLogger(logfile=logfile)
-    cli = MockClient(logger=logger)
+    logger = TranslationLogger(logfile=logfile)
+    cli = MockClient(logger=logger, model='gpt-4.1-2077-01-01')
+
     task = TranslationTask(
         target_pairs=pairs,
         dm=dm,
@@ -165,71 +114,50 @@ def test_logging_with_manual_retry():
         logger=logger,
         mt_folder=test_folder,
         num_of_sents=400,
-    )
-    setup_teardown(test_folder, lambda: silent_task_run(task.run))
-    target_pair = [pairs[1], pairs[-1]]
-    logvalues = logfile.getvalue()
-    log_data = [json.loads(ln) for ln in logvalues.splitlines()]
-    log_ids = [log['translation']['id'] for log in log_data]
-    target_ids = [log_ids[1], log_ids[-1]]
-    retry = RetryLog(pairs=target_pair, log_ids=target_ids,
-                     reasons=['Returned Src text', 'BLEU score single digit'])
-    new_logger = MyLogger(logfile=logfile, retry=retry)
-    cli = MockClient(logger=new_logger)
-    task = TranslationTask(
-        target_pairs=target_pair,
-        dm=dm,
-        client=cli,
-        logger=new_logger,
-        mt_folder=test_folder,
-        num_of_sents=400,
-        manual_retry=True,
-    )
-    setup_teardown(test_folder, lambda: silent_task_run(task.run))
-    logvalues = logfile.getvalue()
-    log_data = [json.loads(ln) for ln in logvalues.splitlines()]
-    expected_logs = len(pairs) + 2
-    assert len(log_data) == expected_logs
-    assert log_data[-1]['manual_retry']['reason'] == 'BLEU score single digit'
-    assert log_data[-2]['manual_retry']['reason'] == 'Returned Src text'
-
-
-def test_logging_with_scenario_1():
-    test_folder = 'tmp_test'
-    pairs = get_pairs(2)
-    dm = Opus100Manager()
-    logfile = StringIO()
-    logger = MyLogger(logfile=logfile)
-    scenario = [0, 1, 2, 0]
-    cli = MockClient(logger=logger, scenario=scenario)
-    task = TranslationTask(
-        target_pairs=pairs,
-        dm=dm,
-        client=cli,
-        logger=logger,
-        mt_folder=test_folder,
-        num_of_sents=400,
-        max_retries=2,
+        max_retries=1,
         retry_delay=0
     )
     setup_teardown(test_folder, lambda: silent_task_run(task.run))
-    logvalues = logfile.getvalue()
-    log_data = [json.loads(ln) for ln in logvalues.splitlines()]
-    assert len(log_data) == len(scenario)
-    assert log_data[0]['verdict']['success'] == 'Translation accepted'
-    assert log_data[1]['verdict']['failure'] == 'Translation rejected'
-    assert log_data[2]['verdict']['failure'] == 'Translation failed'
-    assert log_data[3]['verdict']['success'] == 'Translation accepted'
+    log_data = [json.loads(ln) for ln in logfile.getvalue().splitlines()]
+    assert len(log_data) == 4
+    assert [log['verdict'] for log in log_data] == ['accepted'] * 4
+    assert [log['src_lang'] for log in log_data] == [p[0] for p in pairs]
+    assert [log['tgt_lang'] for log in log_data] == [p[1] for p in pairs]
+    assert [log['id'] for log in log_data] == [
+        f'{task.id}-{i:04d}' for i in range(1, 5)]
+    assert [log['translator']
+            for log in log_data] == ['gpt-4.1-2077-01-01'] * 4
+    assert [log['dataset'] for log in log_data] == [dm.name] * 4
 
 
-def test_logging_with_scenario_2():
+SCENARIOS = {
+    'A': {
+        'dm': Opus100Manager,
+        'pairs': get_sample_pairs(Opus100Manager, k=4),
+        'scenario': [0, 1, 1, 0, 0, 2, 2, 2],
+        'logs': 5,
+        'verdicts': ['accepted', 'rejected', 'rejected', 'accepted', 'accepted'],
+        'max_retries': 2
+    },
+    'B': {
+        'dm': EuroParlManager,
+        'pairs': get_sample_pairs(EuroParlManager, k=4),
+        'scenario': [2, 2, 2, 0, 2, 1, 0, 1, 2, 2],
+        'logs': 4,
+        'verdicts': ['accepted', 'rejected', 'accepted', 'rejected'],
+        'max_retries': 2
+    }
+}
+
+
+def test_logging_with_scenario_A():
     test_folder = 'tmp_test'
-    pairs = get_pairs(3)
-    dm = Opus100Manager()
+    dm = SCENARIOS['A']['dm']()
+    pairs = SCENARIOS['A']['pairs']
     logfile = StringIO()
-    logger = MyLogger(logfile=logfile)
-    scenario = [0, 1, 2, 2, 0, 1, 1, 2, 2]
-    cli = MockClient(logger=logger, scenario=scenario)
+    logger = TranslationLogger(logfile=logfile)
+    cli = MockClient(logger=logger, scenario=SCENARIOS['A']['scenario'])
+
     task = TranslationTask(
         target_pairs=pairs,
         dm=dm,
@@ -237,17 +165,33 @@ def test_logging_with_scenario_2():
         logger=logger,
         mt_folder=test_folder,
         num_of_sents=400,
-        max_retries=3,
+        max_retries=SCENARIOS['A']['max_retries'],
         retry_delay=0
     )
     setup_teardown(test_folder, lambda: silent_task_run(task.run))
-    logvalues = logfile.getvalue()
-    log_data = [json.loads(ln) for ln in logvalues.splitlines()]
-    assert len(log_data) == len(scenario)
-    for idx, s in enumerate(scenario):
-        if s == 0:
-            assert log_data[idx]['verdict']['success'] == 'Translation accepted'
-        elif s == 1:
-            assert log_data[idx]['verdict']['failure'] == 'Translation rejected'
-        elif s == 2:
-            assert log_data[idx]['verdict']['failure'] == 'Translation failed'
+    log_data = [json.loads(ln) for ln in logfile.getvalue().splitlines()]
+    assert len(log_data) == SCENARIOS['A']['logs']
+    assert [log['verdict'] for log in log_data] == SCENARIOS['A']['verdicts']
+
+
+def test_logging_with_scenario_B():
+    test_folder = 'tmp_test'
+    dm = SCENARIOS['B']['dm']()
+    pairs = SCENARIOS['B']['pairs']
+    logfile = StringIO()
+    logger = TranslationLogger(logfile=logfile)
+    cli = MockClient(logger=logger, scenario=SCENARIOS['B']['scenario'])
+    task = TranslationTask(
+        target_pairs=pairs,
+        dm=dm,
+        client=cli,
+        logger=logger,
+        mt_folder=test_folder,
+        num_of_sents=400,
+        max_retries=SCENARIOS['B']['max_retries'],
+        retry_delay=0
+    )
+    setup_teardown(test_folder, lambda: silent_task_run(task.run))
+    log_data = [json.loads(ln) for ln in logfile.getvalue().splitlines()]
+    assert len(log_data) == SCENARIOS['B']['logs']
+    assert [log['verdict'] for log in log_data] == SCENARIOS['B']['verdicts']
