d86cbb2aeb598df0cecb538e15a0f3f0b5e46b6b
na50r <shanrosan@gmail.com>
2025-05-20 17:27:14 +0200

    get scoring ready for bert/comet

--- scripts/scoring.py
+++ scripts/scoring.py
@@ -19,44 +19,16 @@ def compute_chrf(ref: list[str], hyp: list[str]):
     return res.score
 
 
-def compute_comet_kiwi(data, lang):
-    from comet import download_model, load_from_checkpoint
-    model_path = download_model("Unbabel/wmt22-cometkiwi-da")
-    model = load_from_checkpoint(model_path)
-    data = [{'src': o['src'], 'mt': o['mt']} for o in data]
-    model_output = model.predict(data, batch_size=8, gpus=1)
-    print(f'Computed COMET Kiwi scores for src & mt for lang {lang}')
-    return model_output
-
-
-def comet_kiwi_mapper(data, scores):
-    mapping = {}
-    for o, s in zip(data, scores):
-        tup = (o['mt'], o['src'])
-        mapping[tup] = s
-    return mapping
-
-
-def pre_compute_comet_kiwi(data, mapping):
-    scores = []
-    for o in data:
-        tup = (o['mt'], o['src'])
-        s = mapping[tup]
-        scores.append(s)
-    return np.array(scores).mean() * 100
-
-
-def compute_comet(data, lang):
+def compute_comet(data: dict[str, str]):
     from comet import download_model, load_from_checkpoint
     model_path = download_model("Unbabel/wmt22-comet-da")
     model = load_from_checkpoint(model_path)
 
     model_output = model.predict(data, batch_size=8, gpus=1)
-    print(f'Computed COMET scores for ref & hyp of lang {lang}')
     return model_output
 
 
-def comet_mapper(data, scores):
+def comet_mapper(data: dict[str, str], scores: list[float]):
     mapping = {}
     for o, s in zip(data, scores):
         triple = (o['ref'], o['mt'], o['src'])
@@ -64,7 +36,7 @@ def comet_mapper(data, scores):
     return mapping
 
 
-def pre_compute_comet(data, mapping):
+def pre_compute_comet(data: dict[str, str], mapping: dict[tuple[str, str, str], float]):
     scores = []
     for o in data:
         triple = (o['ref'], o['mt'], o['src'])
@@ -78,7 +50,6 @@ def compute_bert_score(ref, hyp, lang):
     from bert_score import score
     P, R, F1 = score(hyp, ref, lang=lang, verbose=False,
                      rescale_with_baseline=True)
-    print(f'Computed BERT-F1 scores for ref & hyp of lang {lang}')
     return F1
 
 
@@ -102,12 +73,15 @@ def pre_compute_bert(data, mapping):
 
 
 class ResultProducer:
-    def __init__(self, BLEU_func=compute_bleu, chrF_func=compute_chrf, label2files=None, use_bert=False, use_comet=False, use_comet_kiwi=False):
-        self.BLEU = BLEU_func
-        self.chrF = chrF_func
-        self.BERT = use_bert
-        self.COMET = use_comet
-        self.COMET_KIWI = use_comet_kiwi
+    def __init__(self, label2files: dict[str, str] = None, use_bert: bool = False, use_comet: bool = False):
+        '''
+        Args:
+            label2files: A dictionary that uses labels as keys and filepath to a JSONL file of COMET format {"mt":"sent", "ref":"sent", "src":"sent"}
+            use_bert: A boolean, decide if you want to compute BERT-F1 score or not, not recommended for CPU
+            use_comet: A boolean, decide if you want to compute COMET score or not, not recommended for CPU
+        '''
+        self.use_bert = use_bert
+        self.use_comet = use_comet
         self.label2files = label2files
         self.data_set_sizes = {}
         self.comet_mapping = {}
@@ -117,17 +91,16 @@ class ResultProducer:
         self.comet_scores = []
         self.chrf_scores = []
 
-        self.comet_kiwi_scores = []
-        self.comet_kiwi_mapping = {}
-
     def clear_mappings(self):
         self.comet_mapping = {}
         self.bert_mapping = {}
 
-    def compute_results(self, randomize=False, split_point=None):
+    def compute_results(self, randomize: bool = False, split_point: int = None):
         '''
         Computes the BLEU, chrF, BERT-F1 and Comet scores of a given JSONL file/s of aligned sentences
         Expected format: {src : src_sent, ref : ref_sent, mt : mt_sent} (Based on Comet API specification)
+
+        ASSUME: Label contains suffix of mt/ref language! (For BERT-F1 Score)
 
         Args:
             randomize: Randomizes order of provided sentences
@@ -139,10 +112,9 @@ class ResultProducer:
         self.bleu_scores = []
         self.comet_scores = []
         self.chrf_scores = []
-        self.comet_kiwi_scores = []
 
-        for lang in self.label2files:
-            file = self.label2files[lang]
+        for label in self.label2files:
+            file = self.label2files[label]
             with open(file, 'r') as f:
                 data = [json.loads(ln) for ln in f]
 
@@ -153,51 +125,40 @@ class ResultProducer:
                 assert split_point <= len(data), 'Choose smaller split point'
                 data = data[:split_point]
 
-            self.data_set_sizes[lang] = len(data)
+            self.data_set_sizes[label] = len(data)
             # Store meta data
             mt_sents = [o['mt'] for o in data]
             ref_sents = [o['ref'] for o in data]
             # Eager evaluate BLEU and chrF scores
-            self.bleu_scores.append(self.BLEU(ref_sents, mt_sents))
-            self.chrf_scores.append(self.chrF(ref_sents, mt_sents))
+            self.bleu_scores.append(compute_bleu(ref_sents, mt_sents))
+            self.chrf_scores.append(compute_chrf(ref_sents, mt_sents))
 
             # BERT score optional
             # bert_mapping used for recomputation of sampled or randomized data
             # Avoids encoding the sentences again and again
-            if self.BERT == True:
-                if lang not in self.bert_mapping:
+            if self.use_bert == True:
+                if label not in self.bert_mapping:
+                    lang = label.split('-')[-1]
                     bert_out = compute_bert_score(ref_sents, mt_sents, lang)
                     mapping = bert_mapper(data, bert_out)
-                    self.bert_mapping[lang] = mapping
+                    self.bert_mapping[label] = mapping
 
                     # BERT-F1 score computed as value between 0 to 1
                     # To make Table cleaner, we adhere to BLEU & chrF score and make it value between 0 and 100
                     self.bert_scores.append(np.array(bert_out).mean() * 100)
                 else:
                     self.bert_scores.append(pre_compute_bert(
-                        data, self.bert_mapping[lang]))
+                        data, self.bert_mapping[label]))
 
-            if self.COMET == True:
-                if lang not in self.comet_mapping:
-                    comet_out = compute_comet(data, lang)
+            if self.use_comet == True:
+                if label not in self.comet_mapping:
+                    comet_out = compute_comet(data)
                     mapping = comet_mapper(data, comet_out['scores'])
-                    self.comet_mapping[lang] = mapping
+                    self.comet_mapping[label] = mapping
                     self.comet_scores.append(comet_out['system_score']*100)
                 else:
                     self.comet_scores.append(pre_compute_comet(
-                        data, self.comet_mapping[lang]))
-
-            if self.COMET_KIWI == True:
-                if lang not in self.comet_kiwi_mapping:
-                    comet_out = compute_comet_kiwi(data, lang)
-                    mapping = comet_kiwi_mapper(data, comet_out['scores'])
-                    self.comet_kiwi_mapping[lang] = mapping
-                    self.comet_kiwi_scores.append(
-                        comet_out['system_score']*100)
-                else:
-                    self.comet_kiwi_scores.append(pre_compute_comet_kiwi(
-                        data, self.comet_kiwi_mapping[lang]
-                    ))
+                        data, self.comet_mapping[label]))
 
     def _get_scores(self):
         SCORES = {
@@ -211,38 +172,25 @@ class ResultProducer:
         if len(self.comet_scores) != 0:
             SCORES.update({'COMET': self.comet_scores})
 
-        if len(self.comet_kiwi_scores) != 0:
-            SCORES.update({'COMET_KIWI': self.comet_kiwi_scores})
-
         return SCORES
 
-    def display_results(self, latex=False):
+    def get_scores(self):
         SCORES = self._get_scores()
-        if len(self.bert_scores) != 0:
-            SCORES.update({'BERT-F1': self.bert_scores})
-
-        if len(self.comet_scores) != 0:
-            SCORES.update({'COMET': self.comet_scores})
-
-        if len(self.comet_kiwi_scores) != 0:
-            SCORES.update({'COMET_KIWI': self.comet_kiwi_scores})
         df = pd.DataFrame(SCORES)
+        return df
 
+    def display_results(self, latex: bool = False):
+        df = self.get_scores()
         if latex:
             latex_table = df.to_latex(
                 index=False, float_format="%.2f")
             print(latex_table)
         print(df)
 
-    def store_results(self, output_path):
+    def store_results(self, output_path: str):
         SCORES = self._get_scores()
         df = pd.DataFrame(SCORES)
         df.to_csv(output_path, index=False)
-    
-    def get_scores(self):
-        SCORES = self._get_scores()
-        df = pd.DataFrame(SCORES)
-        return df
 
     def store_mappings(self, output_path: str):
         """
@@ -254,8 +202,7 @@ class ResultProducer:
         # Create mapping dictionary to store
         mappings = {
             'bert_mapping': self.bert_mapping,
-            'comet_mapping': self.comet_mapping,
-            'comet_kiwi_mapping': self.comet_kiwi_mapping
+            'comet_mapping': self.comet_mapping
         }
         # Store mappings
         with open(output_path, 'wb') as f:
@@ -277,19 +224,15 @@ class ResultProducer:
         # Restore mappings
         self.bert_mapping = mappings.get('bert_mapping', {})
         self.comet_mapping = mappings.get('comet_mapping', {})
-        self.comet_kiwi_mapping = mappings.get('comet_kiwi_mapping', {})
 
         print(f"Mappings loaded successfully from {input_path}")
         print(
             f"Loaded BERT mappings for languages: {list(self.bert_mapping.keys())}")
         print(
             f"Loaded COMET mappings for languages: {list(self.comet_mapping.keys())}")
-        print(
-            f"Loaded COMET KIWI mappings for languages: {list(self.comet_kiwi_mapper.keys())}"
-        )
 
 
-def create_matrix_from_csv(path_to_csv, metric='BLEU'):
+def create_matrix_from_csv(path_to_csv: str, metric: str = 'BLEU'):
     # https://stackoverflow.com/questions/72827153/how-to-extract-specific-key-and-value-from-a-dataframe-python
     '''
     Assume CSV of format:
